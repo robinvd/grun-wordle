3e77d3a1ae3b9ac7ecb9de7ce5af383982901494
Robin <robinvandijk@klippa.com>
2025-03-16 16:05:01 +0100

    fix lints

--- src/components/grid/CompletedRow.tsx
+++ src/components/grid/CompletedRow.tsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from 'react'
-
-import { REVEAL_TIME_MS } from '../../constants/settings'
 import { getGuessStatuses } from '../../lib/statuses'
 import { unicodeSplit } from '../../lib/words'
 import { Cell } from './Cell'
@@ -21,29 +18,29 @@ export const CompletedRow = ({
   const statuses = getGuessStatuses(solution, guess)
   const splitGuess = unicodeSplit(guess)
 
-  const [audio_correct] = useState(new Audio('/lingo_correct.mp3'))
-  const [audio_incorrect] = useState(new Audio('/lingo_incorrect.mp3'))
-  const [audio_partial] = useState(new Audio('/lingo_partial.mp3'))
+  // const [audio_correct] = useState(new Audio('/lingo_correct.mp3'))
+  // const [audio_incorrect] = useState(new Audio('/lingo_incorrect.mp3'))
+  // const [audio_partial] = useState(new Audio('/lingo_partial.mp3'))
 
-  useEffect(() => {
-    if (isRevealing && lingoSounds) {
-      for (let i = 0; i < solution.length; i++) {
-        setTimeout(() => {
-          const status = statuses[i]
-          if (status === 'correct') {
-            audio_correct.currentTime = 0
-            audio_correct.play()
-          } else if (status === 'present') {
-            audio_partial.currentTime = 0
-            audio_partial.play()
-          } else if (status === 'absent') {
-            audio_incorrect.currentTime = 0
-            audio_incorrect.play()
-          }
-        }, REVEAL_TIME_MS * i)
-      }
-    }
-  }, [isRevealing])
+  // useEffect(() => {
+  //   if (isRevealing && lingoSounds) {
+  //     for (let i = 0; i < solution.length; i++) {
+  //       setTimeout(() => {
+  //         const status = statuses[i]
+  //         if (status === 'correct') {
+  //           audio_correct.currentTime = 0
+  //           audio_correct.play()
+  //         } else if (status === 'present') {
+  //           audio_partial.currentTime = 0
+  //           audio_partial.play()
+  //         } else if (status === 'absent') {
+  //           audio_incorrect.currentTime = 0
+  //           audio_incorrect.play()
+  //         }
+  //       }, REVEAL_TIME_MS * i)
+  //     }
+  //   }
+  // }, [isRevealing])
 
   return (
     <div className="mb-1 flex justify-center">
